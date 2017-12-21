b9c7845ea0d4d1bc6102f195b2d1e67fb445828f
coolwuu <coolwuu0207@gmail.com>
2017-12-21 10:06:27 +0800

    1st sprint
    1st sprint

---
+++ LunchSystem/LunchDb/dbo/Orders.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Orders](
+	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[MemberName] [nvarchar](20) NOT NULL,
+	[Meal] [nvarchar](50) NOT NULL,
+	[Cost] [int] NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[LastModifiedOn] [datetime] NOT NULL,
+	[Status] [nvarchar](10) NOT NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Orders] ADD  CONSTRAINT [DF_Orders_CreatedOn]  DEFAULT (getdate()) FOR [CreatedOn]
+GO
+
+ALTER TABLE [dbo].[Orders] ADD  CONSTRAINT [DF_Orders_LastModifiedOn]  DEFAULT (getdate()) FOR [LastModifiedOn]
+GO
+
+ALTER TABLE [dbo].[Orders] ADD  CONSTRAINT [DF_Orders_Status]  DEFAULT ('Running') FOR [Status]
+GO
+
+
