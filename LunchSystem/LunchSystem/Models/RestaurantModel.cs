ac97a66acfbad4f12640b48f5bd9d9714dedf03e
coolwuu <coolwuu0207@gmail.com>
2018-01-31 10:26:43 +0800

    Add Restaurant Model

---
+++ LunchSystem/LunchSystem/Models/RestaurantModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LunchSystem.Models
+{
+    public class RestaurantModel
+    {
+        public int RestaurantId { get; set; }
+
+        [Required]
+        public string RestaurantName { get; set; }
+
+        public DateTime LastUploadDateTime { get; set; }
+    }
+}
